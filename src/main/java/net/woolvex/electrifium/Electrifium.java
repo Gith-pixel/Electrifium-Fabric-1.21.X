cca22cfa2005d91ec1130d5572371e9782a7b112
Gith-Pixel <nipunthegod69@gmail.com>
2024-12-15 20:54:59 +0530

    Completed Effect and Potion

--- src/main/java/net/woolvex/electrifium/Electrifium.java
+++ src/main/java/net/woolvex/electrifium/Electrifium.java
@@ -3,12 +3,14 @@ package net.woolvex.electrifium;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.potion.Potions;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.Hand;
@@ -17,6 +19,7 @@ import net.woolvex.electrifium.item.ModArmorMaterials;
 import net.woolvex.electrifium.item.ModItemGroups;
 import net.woolvex.electrifium.item.ModItems;
 import net.woolvex.electrifium.block.ModBlocks;
+import net.woolvex.electrifium.potion.ModPotions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -135,5 +138,8 @@ public class Electrifium implements ModInitializer {
             return TypedActionResult.pass(stack);
         });
 
+		FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
+			builder.registerPotionRecipe(Potions.AWKWARD, Items.SLIME_BALL, ModPotions.RADIATED_POTION);
+		});
 	}
 }
